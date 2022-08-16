4b877731235fcdfa0e71a48fe886cffd8faeb446
Samara Soares Ferreira <samara.sofe@gmail.com>
2022-08-15 21:48:23 -0300

    Mostra a hora de forma dinâmica com SetInterval

--- script.js
+++ script.js
@@ -2,19 +2,24 @@ const PONTEIROHORA = document.querySelector("#hour");
 const PONTEIROMINUTO = document.querySelector("#minute");
 const PONTEIROSEGUNDO = document.querySelector("#second");
 
-var data = new Date();
-console.log(data);
+function executarRelogio() {
 
-let hr = data.getHours();
-let min = data.getMinutes();
-let seg = data.getSeconds();
+    var data = new Date();
+    console.log(data);
 
-console.log("Hora: " + hr + " Minuto: " + min + " Segundo: " + seg);
+    let hr = data.getHours();
+    let min = data.getMinutes();
+    let seg = data.getSeconds();
 
-let posicaoHr = (hr*360/12)+(min*(360/60)/12);
-let posicaoMin = (min*360/60)+(seg*(360/60)/60);
-let posicaoSeg = seg*360/60;
+    console.log("Hora: " + hr + " Minuto: " + min + " Segundo: " + seg);
 
-PONTEIROHORA.style.transform = "rotate(" + posicaoHr + "deg)";
-PONTEIROMINUTO.style.transform = "rotate(" + posicaoMin + "deg)"
-PONTEIROSEGUNDO.style.transform = "rotate(" + posicaoSeg + "deg)"
+    let posicaoHr = (hr * 360 / 12) + (min * (360 / 60) / 12);
+    let posicaoMin = (min * 360 / 60) + (seg * (360 / 60) / 60);
+    let posicaoSeg = seg * 360 / 60;
+
+    PONTEIROHORA.style.transform = "rotate(" + posicaoHr + "deg)";
+    PONTEIROMINUTO.style.transform = "rotate(" + posicaoMin + "deg)"
+    PONTEIROSEGUNDO.style.transform = "rotate(" + posicaoSeg + "deg)"
+}
+
+ var intervalo = setInterval(executarRelogio, 1000);
